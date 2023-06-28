0b6e676c27412b12a2f007de4dd30bed5d81d9ea
Daewoong <dkim94@github.com>
2023-06-28 22:15:58 +0900

    Modify nCk

--- ch06/nCk.cpp
+++ ch06/nCk.cpp
@@ -3,27 +3,31 @@
 
 using std::vector;
 
+int n, k;
+vector<int> picked;
+int count;
 
-int sum = 0;
-
-void cnt_all_combinations(int n, int k, vector<int>& picked) {
-    if (k == 0) sum++;
-    else {
-        int smallest = picked.empty() ? 1 : picked.back() + 1;
-        for (int i = smallest; i <= n; i++) {
-            picked.push_back(i);
-            cnt_all_combinations(n, k-1, picked);
-            picked.pop_back();
+void nck(int n, int k) {
+    if (k == 0) {
+        for (int i=0; i<picked.size(); i++) {
+            printf("%d ", picked[i]);
         }
+        printf("\n");
+        count++;
+        return;
+    }
+    int curr = picked.empty() ? 0 : picked.back() + 1;
+    for (int i=curr; i<n; i++) {
+        picked.push_back(i);
+        nck(n, k-1);
+        picked.pop_back();
     }
 }
 
 int main() {
-    int n, k;
     scanf("%d %d", &n, &k);
-    vector<int> picked;
-
-    cnt_all_combinations(n, k, picked);
-    printf("%d\n", sum);
+    count = 0;
+    nck(n, k);
+    printf("count: %d\n", count);
 }
 
